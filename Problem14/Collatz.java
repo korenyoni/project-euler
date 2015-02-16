dc5cf34fb045779099a38375a7a498b28d4c5915
yonkornilov <yonkornilov@live.com>
2015-02-16 23:45:41 +0000

    Moved programs into folders
    folder names are the programs' repsective problem numbers

--- Problem14/Collatz.java
+++ Problem14/Collatz.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package collatz;
 
 public class Collatz
@@ -63,3 +64,70 @@ public class Collatz
 		return length;
 	}
 }
+=======
+package collatz;
+
+public class Collatz
+{
+	private static final int MAX = 999999;
+	private static int[] collatzLengthArray = new int[MAX];
+	public static void main(String[] args)
+	{
+		populateArray();
+		int maxLength = 0;
+		int currentMax = 0;
+		int maxNumber = 0;
+		for (int number = 900000; number < MAX; number++)
+		{
+			currentMax = getCollatzLength(number);
+			if (currentMax > maxLength)
+			{
+				maxLength = currentMax;
+				maxNumber = number;
+			}
+		}
+		System.out.printf("Maximum length for numbers under %d: %d (%d)\n", MAX, maxLength, maxNumber);
+	}
+	public static void populateArray()
+	{
+		for (int i = 0; i < collatzLengthArray.length; i++)
+		{
+			collatzLengthArray[i] = -1;
+		}
+	}
+	public static int getCollatzLength(int number)
+	{
+		int length = 1;
+		int precalcLength = 0;
+		int originalNumber = number;
+		while (number > 1)
+		{
+			if (number <= collatzLengthArray.length - 1)
+			{
+				precalcLength  = collatzLengthArray[number -1];
+				if (precalcLength != -1)
+				{
+					length += precalcLength -1;
+					System.out.printf("Hit precalcLength for number %d: %d\n", number, precalcLength);
+					collatzLengthArray[originalNumber - 1] = length;
+					return length;
+				}
+			}
+			else
+			{
+				if (number % 2 == 0)
+				{
+					number /= 2;
+				}
+				else
+				{
+					number = (3 * number) + 1;
+				}
+				length++;
+			}
+		}
+		collatzLengthArray[originalNumber - 1] = length;
+		return length;
+	}
+}
+>>>>>>> Added problem 14
--- Problem14/Collatz2.java
+++ Problem14/Collatz2.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package collatz;
 
 import java.util.HashMap;
@@ -49,3 +50,56 @@ public class Collatz2
 		return collatzLength;
 	}
 }
+=======
+package collatz;
+
+import java.util.HashMap;
+
+public class Collatz2
+{
+	private static HashMap<Long, Integer> map = new HashMap<Long, Integer>();
+	public static final int MAX = 1000000;
+	
+	public static void main(String[] args)
+	{
+		int maxLength = 0;
+		long maxNumber = 1;
+		for (long i = 1; i < MAX; i++)
+		{
+			int currentLength = getCollatzLength(i);
+			if (currentLength > maxLength)
+			{
+				maxNumber = i;
+				maxLength = currentLength;
+			}
+		}
+		System.out.printf("The biggest chain is %d and belongs to\nthe number %d", maxLength, maxNumber);
+	}
+	public static int getCollatzLength(long number)
+	{
+		long numberCopy = number;
+		int collatzLength = 1;
+		if (map.containsKey(number))
+		{
+			int value = collatzLength + map.get(number) - 1;
+			map.put(numberCopy, value);
+			return value;
+		}
+		while (number > 1)
+		{
+			//System.out.println(number);
+			if (number % 2 == 0)
+			{
+				number /= 2;
+			}
+			else
+			{
+				number = (number * 3) + 1;
+			}
+			collatzLength++;
+		}
+		map.put(numberCopy, collatzLength);
+		return collatzLength;
+	}
+}
+>>>>>>> Added problem 14
